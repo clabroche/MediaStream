d815770dfa8cef040f56a34ec5807503fc84c282
labroche <corentin.labroche@inist.fr>
2017-09-24 13:03:51 +0200

    make orchestra module to be fully automated

--- script/orchestra-module.js
+++ script/orchestra-module.js
@@ -1,3 +1,4 @@
+const cheerio = require("cheerio");
 const program = require("commander");
 const pkg = require("../package.json");
 const Promise = require("bluebird");
@@ -18,9 +19,12 @@ if (program.moduleName === "none" || program.controller === "none") {
 
 const referencePath = path.resolve("src", "modules", "Reference");
 const modulePath = path.resolve("src", "modules", program.moduleName);
-const modulePathRelative = './src/modules/'
+const modulePathRelative = "./src/modules/";
 const launch = async function() {
+  if (fs.existsSync(modulePath)) throw new Error(`The module ${program.moduleName} exist !`)
+  console.log(`create resources for ${program.moduleName}`)
   await ncp.ncpAsync(referencePath, modulePath);
+  console.log(`rename resources for ${program.moduleName}`);  
   await fs.renameAsync(
     path.join(modulePath, "controller.js"),
     path.join(modulePath, `${program.controller}.js`)
@@ -29,6 +33,7 @@ const launch = async function() {
     path.join(modulePath, "view.html"),
     path.join(modulePath, `${program.moduleName}.html`)
   );
+  console.log(`adapt content for ${program.moduleName}`);  
   const files = await fs.readdirAsync(modulePath);
   const filesToReplace = files.map(async file => {
     const newContent = await fs
@@ -46,11 +51,48 @@ const launch = async function() {
         );
         return fileContent;
       });
-    await fs.writeFileAsync(path.join(modulePath, file), newContent)
+    await fs.writeFileAsync(path.join(modulePath, file), newContent);
   });
-  await fs.appendFileAsync(path.join('src','app.js'), `const ${program.moduleName} = require('${modulePathRelative}${program.moduleName}/app');`)
-  await fs.appendFileAsync(path.join('src','app.js'), `${program.moduleName}.init(ang); \n`)
+  console.log(`link module to app`)
+  await fs.appendFileAsync(
+    path.join("src", "app.js"),
+    `const ${program.moduleName} = require('${modulePathRelative}${program.moduleName}/app');`
+  );
+  await fs.appendFileAsync(
+    path.join("src", "app.js"),
+    `${program.moduleName}.init(ang); \n`
+  );
+  console.log(`link module to navbar view`); 
+  putInNavbar(program)
+  console.log(`link module to home view`);  
+  putInHome(program)
 };
+
+function putInNavbar(program) {
+  const $ = cheerio.load(
+    fs.readFileSync(path.resolve("views", "_navbar.html"))
+  );
+  const $sidebar = $(".sidebar")
+  .append(
+    `  <a class="item" ui-sref="${program.moduleName}" ng-click="changeModule('${program.moduleName}')">\n` +
+    `    <i class="fa fa-picture-o" aria-hidden="true"></i>\n` +
+    `    ${program.moduleName}\n` +
+    `  </a>\n`
+  )
+  .parent();
+  fs.writeFileSync(path.resolve("views", "_navbar.html"), $sidebar.html())
+}
+function putInHome(program) {
+  const $ = cheerio.load(
+    fs.readFileSync(path.resolve("views", "home.html"))
+  );
+  const $home = $(".container")
+    .append(
+      `    <div ui-view="${program.moduleName}"   class="row" ng-if="$state.current.name == '${program.moduleName}'" ></div>\n`
+    )
+    .parent().parent();
+  fs.writeFileSync(path.resolve("views", "home.html"), $home.html());    
+}
 
 function replaceAll(str, find, replace) {
   return str.replace(new RegExp(escapeRegExp(find), "g"), replace);
@@ -59,8 +101,10 @@ function escapeRegExp(str) {
   return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
 }
 
-launch().then(_=>{
-    console.log('done !')
-}).catch(err => {
-  console.log(err);
-});
+launch()
+  .then(_ => {
+    console.log("done !");
+  })
+  .catch(err => {
+    console.log(err);
+  });
