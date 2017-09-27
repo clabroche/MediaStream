37987b51d59ba5e49f456a911cdf5ad06fc75a51
labroche <corentin.labroche@inist.fr>
2017-09-27 04:29:56 +0200

    fix path to db

--- src/modules/music/src/musicDB.js
+++ src/modules/music/src/musicDB.js
@@ -1,13 +1,15 @@
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
+var path = require("path");
+var appDir = path.dirname(require.main.filename);
 const _ = require('lodash');
-if (!fs.existsSync('./db/music.db')) { fs.writeFileSync('./db/music.db', '{}', 'utf8'); }
-let dbFile = JSON.parse(fs.readFileSync('./db/music.db', 'utf8'));
+if (!fs.existsSync(path.resolve(appDir, 'db/music.db'))) { fs.writeFileSync(path.resolve(appDir, 'db/music.db'), '{}', 'utf8'); }
+let dbFile = JSON.parse(fs.readFileSync(path.resolve(appDir, 'db/music.db'), 'utf8'));
 
 function MusicDB () {
   this.merge = function (data) {
     dbFile = _.merge(dbFile, data);
-    fs.writeFileSync('./db/music.db', JSON.stringify(dbFile));
+    fs.writeFileSync(path.resolve(appDir, 'db/music.db'), JSON.stringify(dbFile));
   };
   this.getArtists = () => _.values(dbFile.artists);
   this.albums = artist => _.values(dbFile.artists[artist].albums);
